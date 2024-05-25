04775a46b0497b4e60086e53d7c458b4fb1dc413
shivanksinghmch <singh@mch.nl>
2024-05-25 18:37:27 +0200

    code fix for Depends data missing value

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from main import PackageManager
 
 
 app = Flask(__name__)
+package_manager = PackageManager()
+
+@app.route('/packages', methods=['GET'])
+def get_packages():
+    package_names = [pkg['Package'] for pkg in package_manager.packages_list]
+    return jsonify(package_names)
 
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 import json
-
-class Transform:
+import re
+class PackageManager:
 
     def __init__(self):
         self.status_file_path = 'status'
         self.json_output_path = 'packages.json'
-        self.packages_list = []
-        self.package_dict = {}
+        self.packages_list, self.package_dict = self.create_package_from_file()
 
     def create_package_from_file(self):
-
+        packages_list = []
+        package_dict = {}
         with open(self.status_file_path, 'r') as file:
             package_data = {}
             for line in file:
                 line = line.strip()
                 if line == "":
                     if package_data:
-                        self.packages_list.append(package_data)
-                        self.package_dict[package_data['Package']] = package_data
+                        packages_list.append(package_data)
+                        package_dict[package_data['Package']] = package_data
                         package_data = {}
                 else:
                     if ':' in line:
                         key, value = line.split(':', 1)
                         if key in ['Package', 'Description', 'Depends']:
                             if key == 'Depends':
-                                package_data[key.strip()] = list(set(dep.split()[0] for dep in value.split(', ')))
+                                package_data[key.strip()] = [ dep.split()[0] for dep in re.split(r'[,\|]',value)]
                             else:
                                 package_data[key.strip()] = value.strip()
                     else:
                         if 'Description' in package_data:
                             package_data['Description'] += '\n' + line
                         else:
-                            print(f"Warning: unexpected format in line: {line}")
+                            pass
             if package_data:  # last line of the file
-                self.packages_list.append(package_data)
-                self.package_dict[package_data['Package']] = package_data
+                packages_list.append(package_data)
+                package_dict[package_data['Package']] = package_data
 
-        return self.packages_list, self.package_dict
+        return packages_list, package_dict
 
     def convert_to_json(self, data):
         with open(self.json_output_path, 'w') as file:
@@ -44,10 +44,9 @@ class Transform:
 
 
 if __name__ == '__main__':
-
-    transform = Transform()
-    packages_list, package_dict = transform.create_package_from_file()
-    transform.convert_to_json(package_dict)
+    package_manager = PackageManager()
+    print(package_manager.packages_list[0])
+    package_manager.convert_to_json(package_manager.package_dict)
 
 
 
