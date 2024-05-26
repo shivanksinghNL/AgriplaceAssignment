055cdb1b36db8275c9ae4a5e942282e1f658be78
shivanksinghmch <singh@mch.nl>
2024-05-26 22:10:29 +0200

    develop api for GET package list  and  package details

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from main import PackageManager
-
 
 app = Flask(__name__)
 package_manager = PackageManager()
@@ -8,8 +7,22 @@ package_manager = PackageManager()
 @app.route('/packages', methods=['GET'])
 def get_packages():
     package_names = [pkg['Package'] for pkg in package_manager.packages_list]
-    return jsonify(package_names)
+    return jsonify({"packages": package_names} )
 
+@app.route('/packages/<package_name>', methods=['GET'])
+def get_package_details(package_name):
+    package = package_manager.package_dict.get(package_name)
+    if not package:
+        abort(400, "Package name is not correct")
+
+    details = {
+        'name': package.get('Package'),
+        'description': package.get('Description'),
+        'dependencies': package_manager.get_dependencies(package_name),
+        'reverse_dependencies': package_manager.get_reverse_dependencies(package_name)
+    }
+
+    return jsonify(details)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class PackageManager:
         self.status_file_path = 'status'
         self.json_output_path = 'packages.json'
         self.packages_list, self.package_dict = self.create_package_from_file()
+        self.host_url = "http://127.0.0.1:5000"
 
     def create_package_from_file(self):
         packages_list = []
@@ -42,11 +43,35 @@ class PackageManager:
         with open(self.json_output_path, 'w') as file:
             json.dump(data, file, indent=2)
 
+    def get_dependency_url(self, dependency):
+        URL = self.host_url + "/packages/{}"
+        return URL.format(dependency) if dependency in self.package_dict.keys() else "Not Installed"
 
-if __name__ == '__main__':
-    package_manager = PackageManager()
-    print(package_manager.packages_list[0])
-    package_manager.convert_to_json(package_manager.package_dict)
+    def get_dependencies(self, package_name: str):
+        package = self.package_dict.get(package_name)
+        if 'Depends' in package:
+            dependencies = [
+                {
+                    'name': dependency,
+                    'details_url': self.get_dependency_url(dependency)
+                } for dependency in package['Depends']
+            ]
+            return dependencies
+        return []
+
+    def get_reverse_dependencies(self, package_name: str):
+        reverse_deps = []
+        for package in self.packages_list:
+            if 'Depends' in package:
+                # dependencies = [dep.split()[0] for dep in package['Depends'].split(', ')]
+                if package_name in package['Depends']:
+                    reverse_deps.append( { 'name' : package['Package'], 'details_url': self.get_dependency_url(package['Package'])} )
+        return reverse_deps
+
+# if __name__ == '__main__':
+#     package_manager = PackageManager()
+#     print(package_manager.packages_list[0])
+#     package_manager.convert_to_json(package_manager.package_dict)
 
 
 
